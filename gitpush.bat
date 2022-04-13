c539b503bd63a2fffd088b8ba53af83ea7dd89a4
kyoungsup <sks8803@gmail.com>
2022-04-12 17:50:38 -0700

    04/1/222

---
+++ gitpush.bat
@@ -0,0 +1,9 @@
+cd C:/Users/qcells/Desktop/ATP/US_ATP_BI
+
+git init
+git pull https://github.com/KyoungsupShin/US_ATP_BI.git dev
+git add . 
+git commit -m "20220412"
+git push
+
+timeout /t 10 /nobreak
