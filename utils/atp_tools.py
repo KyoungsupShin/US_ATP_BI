de12aefa2409c415977a62eb0a89635b6c50470e
kyoungsup <sks8803@gmail.com>
2022-10-25 19:00:48 -0700

    10/25/2022 Tue

--- utils/atp_tools.py
+++ utils/atp_tools.py
@@ -5,94 +5,8 @@ from email_tools import *
 import warnings
 warnings.filterwarnings("ignore")
 
-<<<<<<< HEAD:utils/atp_tools.py
 class Ending_On_Hand(Outlook_Utils): #Overall Process: get atp data -> iteration key values(item_Code, wh_location, date) -> processing -> insert data
     def get_atp_data(self): #read atp data
-=======
-sql = '''
-with t1 as (
-    select ISNULL(WH_Location, 'MISS')  AS WH_Location, Item_Code, SUM(ON_HAND) AS ON_HAND from (
-    select WH_Location , Item_Code , SUM(PCS) AS ON_HAND from ON_HAND_SAP ohs 
-    where Updated_Date = (select max(Updated_Date) from ON_HAND_SAP )
-    group by WH_Location , Item_Code
-
-    UNION 
-
-    select distinct WH_Location, Item_Code ,0 AS ON_HAND from ATP_BI_HISTORY_SUMMARY abh
-    where Item_Code is not NULL 
-    ) t1
-    group by WH_Location, Item_Code
-)
-select 
-	CONVERT(Date, 날짜Key) AS Dates
-	,t1.*
-	, 0  as Intake 
-	, 0 as Soft_Allocation 
-	, 0 as Hard_Allocation 
-	, 0 as CPO_shipped 
-from t1, BI_CALENDAR bc 
-WHERE 날짜Key between (
-						select MAX(Based_Date) from ON_HAND_SAP ohs 
-						where Updated_Date = (select max(Updated_Date) from ON_HAND_SAP )) 
-					and DATEADD(dd, 240, getdate())
-'''
-
-sql2 = '''
-select 날짜 as Dates
-	, ISNULL(WH_Location, 'MISS') AS WH_Location 
-	, Item_Code 
-	, 0 AS ON_HAND
-	, Intake_Shipments_FCST + Intake_Shipments_ACTUAL  as Intake 
-	, Soft_Allocation * -1 AS Soft_Allocation
-	, Hard_Allocation * -1 AS Hard_Allocation
-	, CPO_shipped * -1 AS CPO_shipped 
-from ATP_BI_HISTORY_SUMMARY abh 
-WHERE 날짜 between (
-						select MAX(Based_Date) from ON_HAND_SAP ohs 
-						where Updated_Date = (select max(Updated_Date) from ON_HAND_SAP ))
-				and DATEADD(dd, 240, getdate())
-'''
-
-sql3 = '''
-    SELECT 
-        CONCAT(t1.Item_Code, tc.Code) AS Item_Code
-        , t1.ProductName
-        , t1.Power_Class
-        , t1.SegmentName
-        , tc.Rate AS Tariff FROM (
-        select 
-            Distinct DummyID AS Item_Code
-            , SUBSTRING(Product_Name, 
-                        CHARINDEX( '/', Product_Name ,0) + 2, 
-                        CHARINDEX( '/', Product_Name , CHARINDEX( '/', Product_Name ,0) + 2) - (CHARINDEX( '/', Product_Name ,0) + 3)) 
-                        AS ProductName 
-            , Power_Class
-            , CASE WHEN Segment = 'ULC' THEN 'U&C' WHEN Segment = 'RES' THEN 'Residential' END AS SegmentName
-        from POWER_CLASS_DIST aap 
-    ) as t1 , TARIFF_CODE tc 
-    UNION ALL
-    select icms.Item_Code , icms.ProductName, icms.Power_Class, SegmentName , ISNULL(tc2.Rate,0) AS Tariff  from ITEM_CODE_MASTER_SAP_TARIFF icms
-	left join TARIFF_CODE tc2 on RIGHT(icms.Item_Code,1) = tc2.Code  
-'''
-
-sql4 = '''
-    select 
-            distinct WH_Location 
-            , WestEast
-    from WAREHOUSE_INFO wi with(nolock)
-    LEFT JOIN US_STATES_CODE usc 
-    ON LEFT(wi.WH_Location,2) = LEFT(usc.StatesCode,2)
-'''
-
-sql5 = '''
-    select Code, Rate AS Tariff 
-    from TARIFF_CODE tc 
-'''
-
-class Ending_On_Hand(Email_Utils):
-    def get_atp_data(self):
-        self.connect_azuredb()
->>>>>>> 7113aacbfbc698005dbb596d6c575614259ea299:utils/ending_onhand.py
         self.df_onhand = self.fetch_data(sql)
         self.df_atp = self.fetch_data(sql2)
         self.df_itemcode = self.fetch_data(sql3)
@@ -193,6 +107,7 @@ class Ending_On_Hand(Email_Utils):
                     return df_ending_onhand_result_join[df_ending_onhand_result_join['RANK_YYYY']==2].YYYY_BI.max()
             else:
                 return x
+                
         self.get_atp_data()
         df_ending_onhand_result = pd.DataFrame()
         for self.ic in pd.concat([self.df_atp['Item_Code'], self.df_onhand['Item_Code']]).unique():        
