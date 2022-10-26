b3de630799d633d3c3fc8dfd3f0e56fe444c8235
kyoungsup <sks8803@gmail.com>
2022-10-25 18:37:10 -0700

    10/25/2022 Tue

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd C:/Users/qcells/Desktop/US_ATP_BI
 set mytime=%date%
 
 git init
-git pull https://github.com/KyoungsupShin/US_ATP_BI.git dev
+git pull https://github.com/KyoungsupShin/US_ATP_BI.git dev2
 git add . 
 git commit -m "%mytime%" 
 git push
